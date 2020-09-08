05e47084401261e33a7ec2f50f924b19aa0ae227
Phuuu <37841969+Phuuu@users.noreply.github.com>
2020-09-08 10:34:17 +0100

    Image changes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,35 +10,35 @@ function Cards() {
                 <div className='cards__wrapper'>
                     <ul className="cards__items">
                         <CardItem 
-                            src='images/img-9.jpg'
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
+                            src='images/img-6.png'
+                            text='Rockford Fosgate P1 single loaded enclosure, powered by a JBL GX-A3001 mono amplifier'
+                            label='Car Audio'
                             path='/services'
                         />
                         <CardItem 
-                            src='images/img-2.jpg'
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
+                            src='images/img-4.png'
+                            text='All in the details, Thinking of upgrading your wheels?'
+                            label='Rotiform'
                             path='/products'
                         />
                     </ul>
                     <ul className="cards__items">
                         <CardItem 
-                            src='images/img-1.jpg'
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
+                            src='images/img-1.png'
+                            text='Our Mk7.5 Golf GTI on @air_lift_performance & cast @rotiform HUR'
+                            label='Gallery'
                             path='/services'
                         />
                         <CardItem 
-                            src='images/img-3.jpg'
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
+                            src='images/img-2.png'
+                            text='Audi A4 on @air_lift_performance & cast @rotiform KPS'
+                            label='Gallery'
                             path='/products'
                         />
                         <CardItem 
-                            src='images/img-3.jpg'
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
+                            src='images/img-3.png'
+                            text='M2 Competition on @air_lift_performance & cast @rotiform OZR⁠'
+                            label='Gallery'
                             path='/services'
                         />
                     </ul>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,9 +6,9 @@ import './HeroSection.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='videos/video-1.mp4' autoPlay loop muted />
-            <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for?</p>
+            <video src='videos/video-2.mp4' autoPlay loop muted />
+            <h1>CAR AUDIO AND SECURITY</h1>
+            <p>UK's #1 Car Audio and Air Suspension Distributor</p>
             <div className='hero-btns'>
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Get Started</Button>
                 <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Watch Trailer <i className='far fa-play-circle'></i></Button>
